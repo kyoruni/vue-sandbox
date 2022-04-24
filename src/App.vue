8326507e834e85d611189d4aff5a265fa980e3b7
kyoruni <40832190+kyoruni@users.noreply.github.com>
2022-04-24 17:57:08 +0900

    :wrench: Headerをコンポーネントに切り出し

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,30 @@
 <template>
-  <div id="app" class="container-fluid">
-    <div class="row">
-      <div class="col-sm-2 p-0">
-        <left/>
-      </div>
-      <div class="col-sm-10 p-0">
-        <right/>
+  <div id="app">
+    <Header/>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-2 p-0">
+          <left/>
+        </div>
+        <div class="col-sm-10 p-0">
+          <right/>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header'
 import Left from '@/components/Left'
 import Right from '@/components/Right'
 
 export default {
   name: "App",
   components: {
+    Header,
     Left,
-    Right
+    Right,
   }
 }
 </script>
@@ -27,5 +32,8 @@ export default {
 <style scoped>
 #app {
   background: pink;
+}
+.container-fluid{
+  overflow: hidden;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="header">
+    <b-navbar type="dark" variant="info">
+      <b-navbar-brand href="#">vue-sandbox</b-navbar-brand>
+    </b-navbar>
+  </div>
+</template>
