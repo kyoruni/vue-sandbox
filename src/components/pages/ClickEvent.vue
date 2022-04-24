35bf6b2336ae4709d74c131a3b51321483d3f0fb
kyoruni <40832190+kyoruni@users.noreply.github.com>
2022-04-24 19:36:06 +0900

    :wrench: クリックイベント：HPの最大値を表示

--- src/components/pages/ClickEvent.vue
+++ src/components/pages/ClickEvent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="click-event p-2">
     <h2>click event</h2>
-    <h3>残りHP：{{ hp }}</h3>
+    <h3>残りHP：{{ hp }} / {{ this.maxHp }}</h3>
     <b-alert show variant="info" v-if="showAlertInfo">{{ message }}</b-alert>
     <b-alert show variant="danger" v-if="showAlertDanger">{{ message }}</b-alert>
     <b-alert show variant="success" v-if="showAlertSuccess">{{ message }}</b-alert>
