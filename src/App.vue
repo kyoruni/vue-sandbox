1c70c4b30269b2f01d1a7debb01a3fd97f60a86e
kyoruni <40832190+kyoruni@users.noreply.github.com>
2022-04-24 17:10:36 +0900

    :wrench: bootstrap導入

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button class="btn btn-info">bootstrap test</button>
+    <b-icon-exclamation-triangle-fill></b-icon-exclamation-triangle-fill>
   </div>
 </template>
 
